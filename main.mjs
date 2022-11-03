e54064d597ffe3872dddab3811d5b09bef7c7420
William Pheloung <wpheloung@gmail.com>
2022-11-03 15:25:16 -0700

    init

---
+++ main.mjs
@@ -0,0 +1,11 @@
+import { getObjectsByPrototype } from 'game/utils';
+import { Creep } from 'game/prototypes';
+import { Flag } from 'arena/season_alpha/capture_the_flag/basic';
+
+export function loop() {
+    var enemyFlag = getObjectsByPrototype(Flag).find(object => !object.my);
+    var myCreeps = getObjectsByPrototype(Creep).filter(object => object.my);
+    for(var creep of myCreeps) {
+        creep.moveTo(enemyFlag);
+    }
+}
